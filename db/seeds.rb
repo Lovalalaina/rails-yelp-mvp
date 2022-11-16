3032b80a943c60238e3f02fa6f3b427c07fdae64
Miarinkaja Lovalalaina <lovalalaina@gmail.com>
2022-11-16 16:44:43 +0100

    seeds ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoomA = {name: "DishoomA", address: "7 Boundary St, London E2 7JE", category:"french", phone_number: '04911234565'}
+dishoomB = {name: "DishoomB", address: "9 Boundary St, London E2 7JE", category:"belgian", phone_number: '04911234566'}
+dishoomC = {name: "DishoomC", address: "11 Boundary St, London E2 7JE", category:"italian", phone_number: '04911234567'}
+dishoomD = {name: "DishoomD", address: "13 Boundary St, London E2 7JE", category:"chinese", phone_number: '04911234568'}
+dishoomE = {name: "DishoomE", address: "15 Boundary St, London E2 7JE", category:"japanese", phone_number: '04911234569'}
+dishoomF = {name: "DishoomF", address: "17 Boundary St, London E2 7JE", category:"french", phone_number: '04911234562'}
+
+[dishoomA, dishoomB, dishoomC, dishoomD, dishoomE, dishoomF].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
