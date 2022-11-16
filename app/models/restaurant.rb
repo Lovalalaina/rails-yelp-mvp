6923a465ce142a3e4227fa40ed21b95523e902bc
Miarinkaja Lovalalaina <lovalalaina@gmail.com>
2022-11-16 16:08:49 +0100

    1ere etapes ok

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,20 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: { in: CATEGORY, message: "%{value} is not a valid category" }
 end
+
+
+
+# validates_length_of :phone_number, in: 7..16, allow_blank: true
+# class Pet < ApplicationRecord
+#   SPECIES = ['turtle', 'cat', 'dog', 'licorn']
+
+#   validates :name, presence: true
+#   validates :species, inclusion: { in: SPECIES,
+#     message: "%{value} is not a valid specie" }
+# end
